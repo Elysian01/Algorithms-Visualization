ce86fc14e7db72da65e5f6b1e42526cb03aeaf67
Abhishek Gupta <abhig0209@gmail.com>
2020-08-26 01:34:31 +0530

    frontend completed

--- js/pathfinding/init.js
+++ js/pathfinding/init.js
@@ -1,17 +1,39 @@
 // draw Board
 setInterval(draw, 10);
-canvas.onmousedown = mouseDown
-canvas.onmouseup = mouseUp
+canvas.onmousedown = mouseDown;
+canvas.onmouseup = mouseUp;
 
 //reset grid
 document.getElementById("reset").addEventListener("click", reset);
 
 // Maze
 document.getElementById("random-maze").addEventListener("click", randomMaze);
-document.getElementById("rectangular-maze").addEventListener("click", recursiveDivision);
+document
+    .getElementById("rectangular-maze")
+    .addEventListener("click", recursiveDivision);
+
+// Visualize Button
+document.getElementById("visualize").addEventListener("click", visualize);
+
+function visualize() {
+    let algoName = document.getElementById("algo").value;
+    if (algoName === "") {
+        alert("Please Choose the Algorithm First !");
+    } else if (algoName == "bfs") {
+        console.log(algoName);
+    } else if (algoName == "dfs") {
+        console.log(algoName);
+    } else if (algoName == "astar") {
+        //astar
+    } else if (algoName == "dijkstra") {
+        console.log(algoName);
+    } else if (algoName == "bidir") {
+        console.log(algoName);
+    }
+}
 
 //algorithms call
 function algoChange() {
-    let algo = document.getElementById("algo").value;
-    // console.log(algo);
+    let algoName = document.getElementById("algo").value;
+    // console.log(algoName);
 }
