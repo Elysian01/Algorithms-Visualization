1c5b7869d7a15361638e2b17ca7d55ad1c6766bd
Abhishek Gupta <abhig0209@gmail.com>
2020-09-01 18:55:11 +0530

    Searching Page Completed

--- js/searching/searching.js
+++ js/searching/searching.js
@@ -1,66 +1,17 @@
-const resetBtn = document.getElementById("reset");
-const searchBtn = document.getElementById("search");
+var size, speed;
+var delay = 2500;
+
+var heading = document.querySelector(".heading");
+var algoName = document.getElementById("algo").value;
 const result = document.querySelector(".result");
+document.getElementById("search").addEventListener("click", search);
+document.getElementById("reset").addEventListener("click", reset);
 
-var arr = [];
-
-let delay = 750;
-
-function init(elements = 10, speed = 750) {
+function init(elements = 30, speed = 750) {
     addCard(elements);
     delay = speed;
     result.style.display = "none";
-    sortResetArray();
-}
-
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function linearSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-    return arr;
-}
-
-function sortResetArray() {
-    arr = [];
-    console.log("Array reset called !");
-    blocks = document.querySelectorAll(".card");
-    indexs = document.querySelectorAll(".index");
-    for (let i = 0; i < blocks.length; i++) {
-        let num = randomIntFromInterval(5, 100);
-        //    document.getElementById(i).innerText = num;
-        arr.push(num);
-        blocks[i].style.backgroundColor = "#003b46";
-        indexs[i].style.backgroundColor = "#5bc8ac";
-        indexs[i].style.color = "#003b46";
-    }
-    arr = linearSort(arr);
-    for (let i = 0; i < blocks.length; i++) {
-        document.getElementById(i).innerText = arr[i];
-    }
-    result.style.display = "none";
-    document.getElementById("searchKey").value = "";
-}
-
-function visualize() {
-    const elements = document.getElementById("elements").value;
-    const speed = document.getElementById("speed").value;
-    init(elements, speed);
-}
-
-function jumpVisualize() {
-    visualize();
-    const m = document.getElementById("m-value");
-    m.innerText = Math.floor(Math.sqrt(arr.length));
+    linearResetArray();
 }
 
 function addCard(elements = 10) {
@@ -68,6 +19,149 @@ function addCard(elements = 10) {
     container.innerHTML = "";
     for (let i = 0; i < elements; i++) {
         container.innerHTML += `<div class="column"><div class="card"><span id="${i}">56</span><span class="index">${i}</span></div></div>`;
+    }
+}
+
+async function search() {
+    if (algoName === "") {
+        alert("Please Select Algorithm First !");
+    } else {
+        console.log(algoName);
+        if (algoName == "linear") {
+            linearSearch();
+        }
+        if (algoName == "jump") {
+            jumpSearch();
+        }
+        if (algoName == "binary") {
+            binarySearch();
+        }
+        if (algoName == "exponent") {
+            exponentSearch();
+        }
+    }
+}
+
+
+function reset() {
+    if (algoName === "") {
+        alert("Please Select Algorithm First !");
+    } else {
+        console.log(algoName);
+        if (algoName == "linear") {
+            linearResetArray();
+        } else {
+            sortResetArray();
+        }
+    }
+
+}
+
+
+function sizeChange() {
+    size = document.getElementById("size").value
+    searchKeyValue = document.getElementById("searchKey").value;
+    init(size, speed)
+    document.getElementById("searchKey").value = searchKeyValue
+}
+
+function speedChange() {
+    speed = document.getElementById("speed").value
+    searchKeyValue = document.getElementById("searchKey").value;
+    delay = 1500;
+    delay = parseInt(delay / speed);
+    init(size, delay)
+    document.getElementById("searchKey").value = searchKeyValue
+}
+
+function headerDisplay(algoName) {
+    if (algoName == "linear") {
+        heading.innerHTML = "Linear Search [O(n)]"
+    } else if (algoName == "jump") {
+        heading.innerHTML = "Jump Search [O(n)]";
+        sortResetArray();
+    } else if (algoName == "binary") {
+        heading.innerHTML = "Binary Search [O(logn)]";
+        sortResetArray();
+    } else if (algoName == "exponent") {
+        heading.innerHTML = "Exponential Search [O(logn)]";
+        sortResetArray();
+    }
+}
+
+function algoSelected() {
+    algoName = document.getElementById("algo").value;
+    headerDisplay(algoName);
+}
+
+function linearResetArray() {
+    arr = [];
+    // console.log("Array reset called !");
+    blocks = document.querySelectorAll(".card");
+    indexs = document.querySelectorAll(".index");
+    for (let i = 0; i < blocks.length; i++) {
+        let num = randomIntFromInterval(5, 100);
+        document.getElementById(i).innerText = num;
+        arr.push(num);
+        blocks[i].style.backgroundColor = "#003b46";
+        indexs[i].style.backgroundColor = "#5bc8ac";
+        indexs[i].style.color = "#003b46";
+    }
+    result.style.display = "none";
+    document.getElementById("searchKey").value = "";
+}
+
+async function linearSearch() {
+    let found = false;
+    const searchKey = document.getElementById("searchKey").value;
+
+    if (searchKey === "") {
+        alert("Please Enter number you have to find");
+    } else {
+        document.getElementById("searchKey").disabled = true;
+        document.getElementById("search").disabled = true;
+        document.getElementById("reset").disabled = true;
+
+        blocks = document.querySelectorAll(".card");
+        indexs = document.querySelectorAll(".index");
+        for (let i = 0; i < arr.length; i++) {
+            blocks[i].style.backgroundColor = "#5bc8ac";
+            indexs[i].style.backgroundColor = "#003b46";
+            indexs[i].style.color = "#5bc8ac";
+
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, delay)
+            );
+
+            if (arr[i] == searchKey) {
+                result.style.display = "block";
+                result.innerHTML = `Number found at index : ${i}`;
+                console.log(
+                    `${searchKey} Number found at  index ${i} in array `
+                );
+                found = true;
+                break;
+            }
+
+            blocks[i].style.backgroundColor = "#003b46";
+            indexs[i].style.backgroundColor = "#5bc8ac";
+            indexs[i].style.color = "#003b46";
+        }
+        if (!found) {
+            result.style.display = "block";
+            result.innerHTML = "Number not found ";
+        }
+        window.scrollBy({
+            top: 500,
+            behavior: "smooth"
+        });
+        document.getElementById("searchKey").disabled = false;
+        document.getElementById("search").disabled = false;
+        document.getElementById("reset").disabled = false;
+
+        searchKey.value = "";
     }
 }
 
@@ -83,7 +177,6 @@ async function binarySearch() {
         document.getElementById("searchKey").disabled = true;
         document.getElementById("search").disabled = true;
         document.getElementById("reset").disabled = true;
-        document.getElementById("visualize").disabled = true;
 
         blocks = document.querySelectorAll(".card");
         indexs = document.querySelectorAll(".index");
@@ -122,12 +215,14 @@ async function binarySearch() {
         if (!found) {
             result.style.display = "block";
             result.innerHTML = "Number not found ";
-            window.scrollBy({ top: 500, behavior: "smooth" });
+            window.scrollBy({
+                top: 600,
+                behavior: "smooth"
+            });
         }
         document.getElementById("searchKey").disabled = false;
         document.getElementById("search").disabled = false;
         document.getElementById("reset").disabled = false;
-        document.getElementById("visualize").disabled = false;
         searchKey.value = "";
     }
 }
@@ -137,7 +232,6 @@ async function jumpSearch() {
     const n = arr.length;
     let found = false;
     var jumpStep = Math.floor(Math.sqrt(n));
-    m.innerText = jumpStep;
     const searchKey = document.getElementById("searchKey").value;
     let prev = 0;
 
@@ -147,7 +241,6 @@ async function jumpSearch() {
         document.getElementById("searchKey").disabled = true;
         document.getElementById("search").disabled = true;
         document.getElementById("reset").disabled = true;
-        document.getElementById("visualize").disabled = true;
 
         while (arr[Math.min(jumpStep, n) - 1] < searchKey) {
             prev = jumpStep;
@@ -198,11 +291,13 @@ async function jumpSearch() {
             result.style.display = "block";
             result.innerHTML = "Number not found ";
         }
-        window.scrollBy({ top: 500, behavior: "smooth" });
+        window.scrollBy({
+            top: 600,
+            behavior: "smooth"
+        });
         document.getElementById("searchKey").disabled = false;
         document.getElementById("search").disabled = false;
         document.getElementById("reset").disabled = false;
-        document.getElementById("visualize").disabled = false;
         searchKey.value = "";
     }
 }
@@ -220,7 +315,6 @@ async function exponentSearch() {
         document.getElementById("searchKey").disabled = true;
         document.getElementById("search").disabled = true;
         document.getElementById("reset").disabled = true;
-        document.getElementById("visualize").disabled = true;
 
         if (arr[0] == searchKey) {
             blocks[0].style.backgroundColor = "#5bc8ac";
@@ -309,16 +403,70 @@ async function iterativeBinarySearch(arr, left, right, searchKey) {
     if (!found) {
         result.style.display = "block";
         result.innerHTML = "Number not found ";
-        window.scrollBy({ top: 500, behavior: "smooth" });
+        window.scrollBy({
+            top: 500,
+            behavior: "smooth"
+        });
     }
-    window.scrollBy({ top: 500, behavior: "smooth" });
+    window.scrollBy({
+        top: 500,
+        behavior: "smooth"
+    });
     document.getElementById("searchKey").disabled = false;
     document.getElementById("search").disabled = false;
     document.getElementById("reset").disabled = false;
-    document.getElementById("visualize").disabled = false;
     searchKey.value = "";
 }
 
+function linearSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+function sortResetArray() {
+    arr = [];
+    console.log("Array reset called !");
+    blocks = document.querySelectorAll(".card");
+    indexs = document.querySelectorAll(".index");
+    for (let i = 0; i < blocks.length; i++) {
+        let num = randomIntFromInterval(5, 100);
+        //    document.getElementById(i).innerText = num;
+        arr.push(num);
+        blocks[i].style.backgroundColor = "#003b46";
+        indexs[i].style.backgroundColor = "#5bc8ac";
+        indexs[i].style.color = "#003b46";
+    }
+    arr = linearSort(arr);
+    for (let i = 0; i < blocks.length; i++) {
+        document.getElementById(i).innerText = arr[i];
+    }
+    result.style.display = "none";
+    document.getElementById("searchKey").value = "";
+}
+
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setCards(x) {
+    if (x.matches) {
+        init(10);
+    } else {
+        init();
+    }
+}
+
 window.onload = () => {
-    init();
+    var x = window.matchMedia("(max-width: 500px)");
+    setCards(x);
+    x.addListener(setCards);
 };
